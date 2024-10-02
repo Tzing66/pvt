6e725b744a17caee57e49b56ac0229b4544ec29d
Tzing66 <164254504+Tzing66@users.noreply.github.com>
2024-10-01 23:33:18 -0700

    Update classification_using_NN.py
    Added comments for clearer explanation

--- classification_using_NN.py
+++ classification_using_NN.py
@@ -4,11 +4,10 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Path to your local dataset (https://www.kaggle.com/datasets/zalando-research/fashionmnist)
+# Path to local dataset
 train_path = 'C:/Users/tanma/Downloads/archive/fashion-mnist_train.csv'
 test_path = 'C:/Users/tanma/Downloads/archive/fashion-mnist_test.csv'
 
-# Load the CSV files
 train_data = pd.read_csv(train_path)
 test_data = pd.read_csv(test_path)
 
@@ -29,11 +28,27 @@ xtrain = xtrain.reshape(-1, 28, 28)
 xtest = xtest.reshape(-1, 28, 28)
 
 #just to SEE a random test image
-# imgIndex = 9
-# image = xtrain[imgIndex]
-# print("Image Label :",ytrain[imgIndex])
-# plt.imshow(image) 
+imgIndex = 9
+image = xtrain[imgIndex]
+print("Image Label :",ytrain[imgIndex])
+plt.imshow(image) 
 #got an error for "Invalid shape" on the first attempt - resolved using reshape
+
+
+# Here we are defining a SEQUENTIAL NN model using Keras (api from tensorflow). 
+
+#Sequential -> a linear stack of layers, where each layer has exactly one input tensor and one output tensor. Layers are added sequentially, one after the other.
+#Flatten layer -> reshapes a multi-dimensional input into a one-dimensional array. This layer flattens each 28x28 image into a single vector of 784 values (28 * 28 = 784) [this is basically data prep for the next dense layer]
+
+#Dense layer -> (fully connected layer) 300 neurons. 
+# ReLu -> common activation function for hidden layers. It outputs max(0, x), meaning it allows positive values to pass through while setting negative values to zero. Helps introduce non-linearity into the model, allowing it to learn more complex patterns. 
+#parameters (weigths + biases) for relu -> Each neuron in this layer has 784 inputs (from prev layer). Weights: 784 input connections × 300 neurons = 235,200, Each neuron has one bias, so 300 biases. Total Parameters: 235,200 + 300 = 235,500.
+#Second ReLu -> same as last layer and takes 300 inputs (from prev lauyer), total params -> 300 input connections × 100 neurons = 30,000 + 100 biases -> 30,100.
+
+#Output Layer -> The number of neurons here is 10 because this model is built to classify 10 categories (for example, digits 0 to 9 or fashion items with 10 classes). 
+# "softmax" outputs a probability distribution across the 10 classes. Each neuron outputs a value between 0 and 1, and the sum of all the outputs will be 1. The class with the highest probability is the predicted class.
+#params -> 1010 (100 from prev layer x 10 neurons + 10 biases)
+
 
 model = keras.models.Sequential([
     keras.layers.Flatten(input_shape=[28, 28]),
